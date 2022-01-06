db6bb83cf1fae22323e8d96eeddc65b256fc2d49
Yash Joshi <fantastikbruh@gmail.com>
2022-01-06 17:46:32 -0500

    Create "New Entry" page: No functionality

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entry", views.entry, name="entry"),
+    path("wiki/<str:title>", views.wiki, name="wiki")
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,10 +1,26 @@
+from os import name
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title:", max_length=100)
+    break_line = "<br>"
+    content = forms.CharField(label="Content:", widget=forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request):
+    return render(request, "encyclopedia/entry.html", {
+        "form": NewEntryForm()
+    })
+
+def wiki(request, title):
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title,
+        "content": util.get_entry(title)
+    })
